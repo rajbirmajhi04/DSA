ff0e5cf125889e8dbb82459fd9d5cf9873c48b6e
rajbirmajhi <mrajbir96@gmail.com>
2024-06-24 12:00:50 +0530

    Heap data structure completed

--- DS/DS (II)/Heap.cpp
+++ DS/DS (II)/Heap.cpp
@@ -68,13 +68,84 @@ void buildMinHeap(vector <int> &minHeap){
 // Insert into Max Heap
 void maxHeapInsert(vector <int> &heap, int key){
     heap.push_back(key); // Add the new key to the end
+    int i = heap.size() - 1; // index of the new key
 
+    // percolate up
+    // loop continues as long as i is not the root idnex 
+    // and the parent node is less tha the current node
+    while(i != 0 && heap[(i - 1) / 2] < heap[i]){
+        swap(heap[i], heap[(i - 1) / 2]);
+        // index i updated to parent index
+        i = (i - 1) / 2;
+    }
 }
 
 // Insert into Min Heap
 void minHeapInsert(vector <int> &heap, int key){
     heap.push_back(key); // Add the new key to the end
+    int i = heap.size() - 1; // index of the new key
+
+    // percolate up
+    // loop continues as long as i is not the root idnex 
+    // and the parent node is less tha the current node
+    while(i != 0 && heap[(i - 1) / 2] > heap[i]){
+        swap(heap[i], heap[(i - 1) / 2]);
+        // index i updated to parent index
+        i = (i - 1) / 2;
+    }
 }
+
+// Node delete function
+void deleteNode(vector <int> &heap, int index){
+    int n = heap.size();
+
+    if(index >= n){
+        cout << "Index out of bounds! " << endl;
+        return;
+    }
+
+    // if we just removed the last element we are done
+    if (index == n - 1){
+        heap.pop_back();
+        return;
+    }
+    else {
+        // replace the element to be deleted with the last element 
+        heap[index] = heap[n - 1];
+        heap.pop_back();
+    }
+
+    // heapify the node at the given index to maintain the heap property
+    maxHeapify(heap, n - 1, index);
+    minHeapify(heap, n - 1, index);
+
+    // check if percolate up is needed
+    while(index != 0 && heap[(index - 1)/ 2] < heap[index]){
+        swap(heap[index], heap[(index - 1) / 2]);
+        index = (index - 1) / 2;
+    }
+}
+
+// peeking functions
+int peek(vector <int> &heap){
+    if(heap.empty()){
+        throw runtime_error("Heap is empty!");
+    }
+    return heap[0]; // the max or min element is at the root
+}
+
+// extract functions
+int extract(vector <int> &heap){
+    if(heap.empty()){
+        throw runtime_error("Heap is empty!");
+    }
+
+    int elem = heap[0]; // the min or max element is at root
+    heap[0] = heap.back(); // replace root with the last element
+    heap.pop_back(); // remove the last elements
+    return elem;
+}
+
 int main()
 {
     // Init Heap
