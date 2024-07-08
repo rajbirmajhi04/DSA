00c7f96afcd92d77fc0382a2360ec581e1dd1eca
rajbirmajhi <mrajbir96@gmail.com>
2024-07-08 23:53:06 +0530

    Initial

---
+++ Concepts/Dynamic Memory Management/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*
+? malloc(): used to dynamically allcoate a single large block of memory with the specified size.
+? it returns a pointer of type void which can be cast into a pointer of any form.  
+*/
+
+int main()
+{
+    return 0;
+}
