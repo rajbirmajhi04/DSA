2b4faa3fa4dd08951c136b7263311179d69202de
rajbirmajhi <mrajbir96@gmail.com>
2024-07-12 17:24:23 +0530

    completed arrays

--- Concepts/Arrays & Strings/main3.cpp
+++ Concepts/Arrays & Strings/main3.cpp
@@ -2,5 +2,43 @@
 using namespace std;
 int main()
 {
+    // Multidimensional Array
+    int arr[2][3]{
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 3; j++){
+            cout << arr[i][j] << " ";
+        }
+    }
+    cout << endl;
+
+    int row, column;
+    cout << "Enter rows: ";
+    cin >> row;
+    cout << "Enter column: ";
+    cin >> column;
+    
+    int two_dArray[row][column];
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < column; j++){
+            two_dArray[i][j] = (i + 1) * (j + 1); 
+        }
+    }
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < column; j++){
+            cin >> two_dArray[i][j];
+        }
+    }
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < column; j++){
+            cout << two_dArray[i][j] << " ";
+        }
+    }
     return 0;
 }
